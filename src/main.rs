3366e113c8a170eb6a7534a2b8287f231bd26d87
Martijn Berger <martijn.berger@gmail.com>
2023-10-24 21:12:18 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(short, long)]
     profile: String,
 }
